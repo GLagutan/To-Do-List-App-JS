084d2b41b0a7ac4d7c5b78d82f7970ff6872e7bc
ROM <geromelagutan@yahoo.com>
2024-08-08 19:53:23 +0800

    functionalities are added

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ const addTask = () => {
         let li = document.createElement("li")// will create an HTML Element stored inside of li variable
         li.innerHTML = inputBox.value //will display the value of the li variable using innerHTML 
         listContainer.appendChild(li) // this code use the listContainer variable to store the value of li variable
+        let span = document.createElement('span')
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
     }
+    inputBox.value = ""
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+    }
+}, false)
